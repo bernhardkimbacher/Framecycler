e96cba04a0c932b09df61b6769b696d291ee062f
Bernie Kimbacher <bernhard.kimbacher@gmail.com>
2026-07-15 10:21:26 +0200

    fix(win): register PySide6 bin directory with os.add_dll_directory to fix DLL load failure

--- src/framecycler/__init__.py
+++ src/framecycler/__init__.py
@@ -1,4 +1,15 @@
 import os
+import sys
+
+if sys.platform == "win32":
+    try:
+        import PySide6
+        qt_bin_dir = os.path.join(os.path.dirname(PySide6.__file__), "Qt", "bin")
+        if os.path.isdir(qt_bin_dir):
+            os.add_dll_directory(qt_bin_dir)
+    except ImportError:
+        pass
+
 
 # Set default shipped OCIO config if not specified by user
 if "OCIO" not in os.environ:
